aa8a8199dbb20a224f073904f73550f0d62a641f
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-10-16 12:22:22 -0300

    Specifing docker compose file location

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
-sudo docker-compose stop;
+sudo docker-compose -f /home/marco/scih-machine/docker-compose.yaml stop;
 sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/api-gateway:latest;
 sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/gps-service:latest;
 sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/ui-service:latest;
-sudo docker-compose up -d;
+sudo docker-compose -f /home/marco/scih-machine/docker-compose.yaml up -d;
