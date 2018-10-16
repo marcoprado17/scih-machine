6c7f94fc39fa4067f75f1edb47f5eac260a9bd08
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-10-16 11:32:56 -0300

    Adding other services

--- restart.sh
+++ restart.sh
@@ -1,3 +1,5 @@
 sudo docker-compose stop;
 sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/api-gateway:latest;
+sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/gps-service:latest;
+sudo gcloud docker -- pull gcr.io/smart-car-insurance-hybrid/ui-service:latest;
 sudo docker-compose up -d;
